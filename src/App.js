c36decab4cddff92a0deebdc69324a71dc8be13c
DarrenBerg1 <darrenberg50@gmail.com>
2023-07-25 17:16:16 -0500

    started creating media queries to implement mobile responsiveness.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import Homepage from './Pages/HomePage';
 import Navbar from './Components/Navbar';
 import FooterCard from './Components/FooterCard';
-import ContactPage from './Pages/ContactPage';
-import MainMenuPage from './Pages/MainMenuPage';
 import './App.css';
-import StarterPage from './Pages/StarterPage';
-import MainCoursePage from './Pages/MainCoursePage';
-import DrinksPage from './Pages/DrinksPage';
-import DessertPage from './Pages/DessertPage';
-import AboutPage from './Pages/AboutPage';
-import router from './Router';
-import { RouterProvider, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 
 
@@ -21,17 +12,7 @@ function App() {
       <header className="App-header">
         <Navbar />
       </header>
-     
         <Outlet />
-     
-      {/* <AboutPage /> */}
-      {/* <DessertPage /> */}
-      {/* <DrinksPage /> */}
-      {/* <MainCoursePage /> */}
-      {/* <StarterPage /> */}
-      {/* <MainMenuPage /> */}
-      {/* <ContactPage /> */}
-      {/* <Homepage /> */}
       <footer>
        <FooterCard />
       </footer>
