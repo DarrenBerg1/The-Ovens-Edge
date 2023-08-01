d05bb3f6e1c5fbe0cd0a44cbf286e4a5b70639f6
DarrenBerg1 <darrenberg50@gmail.com>
2023-08-01 16:57:22 -0500

    working on the reservation form

--- src/Components/ReservationForm.js
+++ src/Components/ReservationForm.js
@@ -1,53 +1,77 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
+// import { useState } from 'react';
+// import Button from 'react-bootstrap/Button';
+// import Col from 'react-bootstrap/Col';
+// import Form from 'react-bootstrap/Form';
+// import InputGroup from 'react-bootstrap/InputGroup';
+// import Row from 'react-bootstrap/Row';
 
-export default function Reservation() {
-  const [validated, setValidated] = useState(false);
+// export default function Reservation() {
+//   const [validated, setValidated] = useState(false);
 
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+//   const handleSubmit = (event) => {
+//     const form = event.currentTarget;
+//     if (form.checkValidity() === false) {
+//       event.preventDefault();
+//       event.stopPropagation();
+//     }
 
-    setValidated(true);
-  };
+//     setValidated(true);
+//   };
+
+//   return (
+//     <>
+//     <Form noValidate validated={validated} onSubmit={handleSubmit}>
+//       <Row className="mb-3">
+//         <Form.Group as={Col} md="4" controlId="validationCustom01">
+//           <Form.Label>First name</Form.Label>
+//           <Form.Control
+//             required
+//             type="text"
+//             placeholder="First name"
+//             defaultValue="Mark"
+//           />
+//         </Form.Group>
+//         <Form.Group as={Col} md="4" controlId="validationCustom02">
+//           <Form.Label>Email</Form.Label>
+//           <Form.Control
+//             required
+//             type="text"
+//             placeholder="Email"
+//             defaultValue="Otto@demoemail.com"
+//           />
+//         </Form.Group>
+//         <InputGroup>
+//         <InputGroup.Text>Comments</InputGroup.Text>
+//         <Form.Control as="textarea" aria-label="With textarea" />
+//       </InputGroup>
+//       </Row>
+//       <Button type="submit">Submit form</Button>
+//     </Form>
+//     </>
+//   );
+// }
+
+
+import { useForm } from "react-hook-form";
+
+export default function App() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
+  console.log(watch("example")); // watch input value by passing the name of it
 
   return (
-    <>
-    <Form noValidate validated={validated} onSubmit={handleSubmit}>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>First name</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="First name"
-            defaultValue="Mark"
-          />
-        </Form.Group>
-        <Form.Group as={Col} md="4" controlId="validationCustom02">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Email"
-            defaultValue="Otto@demoemail.com"
-          />
-        </Form.Group>
-        <InputGroup>
-        <InputGroup.Text>Comments</InputGroup.Text>
-        <Form.Control as="textarea" aria-label="With textarea" />
-      </InputGroup>
-      </Row>
-      <Button type="submit">Submit form</Button>
-    </Form>
-    </>
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+      <input defaultValue="test" {...register("example")} />
+      
+      {/* include validation with required or other standard HTML validation rules */}
+      <input {...register("exampleRequired", { required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.exampleRequired && <span>This field is required</span>}
+      
+      <input type="submit" />
+    </form>
   );
 }
-
