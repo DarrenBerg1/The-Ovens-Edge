5045b7cbe225550a9fc11a43dbcd943d13136cc0
DarrenBerg1 <darrenberg50@gmail.com>
2023-08-05 10:07:11 -0500

    restructured form, implementing reacthook form and devtool to handle form data.. added validation

--- src/Components/ReservationForm.js
+++ src/Components/ReservationForm.js
@@ -1,82 +1,42 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
+import { useForm } from "react-hook-form";
+import {DevTool} from '@hookform/devtools';
 
-export default function Reservation() {
-  const [validated, setValidated] = useState(false);
 
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
 
-    setValidated(true);
-  };
 
-  return (
-    <>
-    <Form noValidate validated={validated} onSubmit={handleSubmit} id="form-container">
-      <Row className="mb-3, form-card" >
-        <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>First name</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="First name"
-            defaultValue="Mark"
-          />
-        </Form.Group>
-        <Form.Group as={Col} md="4" controlId="validationCustom02">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Email"
-            defaultValue="Otto@demoemail.com"
-          />
-        </Form.Group>
-        <InputGroup>
-        <InputGroup.Text>Comments</InputGroup.Text>
-        <Form.Control as="textarea" aria-label="With textarea" />
-      </InputGroup>
-      </Row>
-      <Button type="submit">Submit form</Button>
-    </Form>
-    </>
-  );
+export default function Reservation(){
+
+const form = useForm(); 
+const {register, control, handleSubmit} = form;
+
+const onSubmit = () => {
+  console.log('Form Submitted')
 }
 
 
-// import { useForm } from "react-hook-form";
+  return (
+    <>
+      <div id="form-container" >
+        <form className="form-card" onSubmit={handleSubmit(onSubmit)} noValidate>
+          <label htmlFor="reservation-name">Name</label>
+          <input type="text" id="reservation-name" {...register("reservation-name", {required: {value: true, message: "name is required."}})} />
 
-// export default function Reservation() {
-//   const { register, handleSubmit, watch, formState: { errors } } = useForm();
-//   const onSubmit = data => console.log(data);
 
-//   console.log(watch("example")); // watch input value by passing the name of it
 
-//   return (
-//     <>
-//     <div id="form-container">
-//     {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
-//     <form onSubmit={handleSubmit(onSubmit)} className="form-card">
-//       {/* register your input into the hook by invoking the "register" function */}
-//       <input defaultValue="test" {...register("example")} />
-      
-//       {/* include validation with required or other standard HTML validation rules */}
-//       <input {...register("exampleRequired", { required: true })} />
-//       {/* errors will return when field validation fails  */}
-//       {errors.exampleRequired && <span>This field is required</span>}
-      
-//       <button >Submit</button>
-//     </form>
+          <label htmlFor="reservation-email">Email</label>
+          <input type="text" id="reservation-email" {...register("reservation-email", {required: "email is required.."})} />
+
+
+
+          <label htmlFor="reservation-comments">Comments</label>
+          <input type="text" id="reservation-comments" {...register("reservation-comments", {required: "Comments are required.."})} />
+
+
+          <button type="submit">Submit</button>
+        </form>
+        <DevTool control={control} />
+      </div>
     
-//     </div>
-//     </>
-//   );
-// } 
+    </>
+  );
+} 
