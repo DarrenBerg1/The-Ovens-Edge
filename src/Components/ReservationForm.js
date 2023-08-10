de01b1b1def197e94a1afb79d6b5bc3ebc587bd7
DarrenBerg1 <darrenberg50@gmail.com>
2023-08-09 20:51:37 -0700

    replaced the reservation page and added modal to navbar

--- src/Components/ReservationForm.js
+++ src/Components/ReservationForm.js
@@ -1,42 +1,15 @@
-import { useForm } from "react-hook-form";
-import {DevTool} from '@hookform/devtools';
+/*
+Add useState to create a drawer to help with reservation functionality, it will not have its own page.
+will function more like a modal.
+will add this code to the navbar page.
 
-
-
+*/
 
 export default function Reservation(){
-
-// const form = useForm(); 
-// const {register, control, handleSubmit} = form;
-
-// const onSubmit = () => {
-//   console.log('Form Submitted')
-// }
 
 
   return (
     <>
-      {/* <div id="form-container" >
-        <form className="form-card" onSubmit={handleSubmit(onSubmit)} noValidate>
-          <label htmlFor="reservation-name">Name</label>
-          <input type="text" id="reservation-name" {...register("reservation-name", {required: {value: true, message: "name is required."}})} />
-
-
-
-          <label htmlFor="reservation-email">Email</label>
-          <input type="text" id="reservation-email" {...register("reservation-email", {required: "email is required.."})} />
-
-
-
-          <label htmlFor="reservation-comments">Comments</label>
-          <input type="text" id="reservation-comments" {...register("reservation-comments", {required: "Comments are required.."})} />
-
-
-          <button type="submit">Submit</button>
-        </form>
-        <DevTool control={control} /> 
-      </div>*/}
-    
     
       <div id="form-container">
 
